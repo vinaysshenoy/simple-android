55e4531697d18abeab3100eec809372ea2f21f73
Vinay Shenoy <vinaysshenoy@gmail.com>
2019-12-20 10:25:19 +0530

    Cache fetched `User` and `Facility`

--- app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffectHandlerDependencies.kt
+++ app/src/main/java/org/simple/clinic/bp/entry/BloodPressureEntryEffectHandlerDependencies.kt
@@ -3,6 +3,7 @@ package org.simple.clinic.bp.entry
 import dagger.Module
 import dagger.Provides
 import org.simple.clinic.facility.Facility
+import org.simple.clinic.functions.CachedFunction0.cached
 import org.simple.clinic.functions.Function0
 import org.simple.clinic.user.LoggedInUserFacilityMapping
 import org.simple.clinic.user.User
@@ -12,20 +13,24 @@ class BloodPressureEntryEffectHandlerDependencies {
 
   @Provides
   fun bindFetchCurrentUser(userDao: User.RoomDao): Function0<User> {
-    return Function0<User> {
+    val supplier = Function0<User> {
       val user = userDao.userImmediate()
       requireNotNull(user)
 
       user
     }
+
+    return cached(supplier)
   }
 
   @Provides
   fun bindFetchCurrentFacility(fetchCurrentUser: Function0<User>, facilityMapping: LoggedInUserFacilityMapping.RoomDao): Function0<Facility> {
-    return Function0 {
+    val supplier = Function0 {
       val user = fetchCurrentUser.call()
 
       facilityMapping.currentFacilityImmediate(user.uuid)
     }
+
+    return cached(supplier)
   }
 }
